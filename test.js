391ff4e48d1eeca5840b36992d5533e1f48ddf2a
NOORHANAN <noorhanawazir123@gmail.com>
2026-09-04 20:00:45 +0500

    third

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+// console.log(typeof"hello");
+
+let score= null;
+console.log(typeof score);
+console.log(score);
+
+let scorechanged= String(score);
+console.log(typeof scorechanged);
+console.log(scorechanged);
+
+
+
+
+
+
+
+
+
+
