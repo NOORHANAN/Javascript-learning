1a66c79e466d8c79f45fba95c604311afbfc5ba7
NOORHANAN <noorhanawazir123@gmail.com>
2026-09-04 21:40:13 +0500

    "fifth"

---
+++ test2.js
@@ -0,0 +1,90 @@
+let score =1;
+const name ="Hanan";
+var truee= true;
+let scorechanged= score + 12; 
+//name="Hanankhan";//error because name is a constant variable and cannot be reassigned
+let middlename;
+let lastname= null;
+
+console.log(typeof score);
+console.log(score);
+console.log(name);
+console.log(typeof name);
+console.log(middlename);
+console.log(typeof lastname);
+console.log(lastname);
+console.log(typeof truee);
+console.log(truee);
+
+
+//...........................
+console.log("score"+1);
+console.log(score+1);
+console.log(scorechanged+1  );
+
+console.log("score"+"1");
+console.log(score+"1");
+
+console.log(lastname+1);
+
+
+console.log(middlename+1);
+//...........................
+let number= 12;
+
+console.log(number*2); 
+console.log(number/2);
+console.log(number%5);
+console.log(number%2);
+console.log(number**2);
+//...........................
+console.log(number=12);
+console.log(number=="12");
+console.log(number==="12");
+console.log(number!=12);
+console.log(number!==12);
+console.log(number != "12");
+console.log(number !== "12");
+console.log(number>12);
+console.log(number>=12);
+console.log(number<12);
+console.log(number<=12);
+
+if(number=11){
+    console.log("number is 11");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
