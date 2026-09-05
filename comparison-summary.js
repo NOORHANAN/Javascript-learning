25ba68514ad75c273f792ec8d646fee89178171f
NOORHANAN <noorhanawazir123@gmail.com>
2026-09-05 14:26:53 +0500

    "7th"

--- comparison-summary.js
+++ comparison-summary.js
@@ -6,7 +6,7 @@ const idd=Symbol("id");
 // console.log(Symbol("id")==Symbol("idd"));
 
 function functionnamefunction() {
-    console.log("function is called");
+    // console.log("function is called");
 }
 
 
@@ -29,18 +29,35 @@ function test2(nane){
     }
     // console.log(nane);
     test2(nane-1);
-     console.log(nane);
+    //  console.log(nane);
 }
 test2(9);
 
 
+let userone={
+    name:"John",
+    age:30,
+};
+// console.log(userone.name);
 
+let usertwo= userone;
+usertwo.name="Peter";
+// console.log(usertwo.name);
+// console.log(userone.name);
 
-
-
-
-
-
-
-
-
+let userthree={
+    name:"noor",
+    age:30,
+};
+let userfour={...userthree};   // spread operator works for copying the object
+userfour.name="Hanan";
+// console.log(userthree.name);
+// console.log(userfour.name);
+let userfive={...userone,...usertwo}; 
+// userfive.name="Spreaded";
+// userfive.age=100;
+// userfive.gender="female";
+//  console.log(userfive.name);
+// console.log(userfive.age);
+// console.log(userfive.gender);
+console.log(userfive);
